89ee597cbf1221e3c21e9c9e585616e174726d0c
kaileeheller <Kaileegirl13@gmail.com>
2015-07-23 18:36:41 +0000

    data groups

--- models/volunteer.rb
+++ models/volunteer.rb
@@ -1,10 +1,30 @@
 class Volunteer
   
   def intialize
-    @organizations= [
-      {:name => "Red Cross", :address => "2221 Chestnut Street Philadelphia , PA 19103", :phone => "(215) 299-4000", :age => "all", :website "http://www.redcross.org/pa/philadelphia/", :tags "people"}
-      ]
+    @organizations= parse
     
+  end
+  def add_organization (name, address, phone, age, website, tags)
+    @organization.push ({:name => name, :address => address, :phone => phone, :age => age, :website => website, :tags => tags})
+  end
+  
+  def parse
+    rows=[]
+    IO.foreach("../public/organizations.csv") do |line| 
+      lst=line.split(",")
+      row={
+        :name => lst[0],
+        :address => lst[1],
+        :phone => lst[2],
+        :age => lst[3],
+        :website => lst[4],
+        :tags => lst[5] }
+      rows.push(row)
+    end
+    return rows
   end
   
 end
+kailee=Volunteer.new
+puts kailee.parse
+
